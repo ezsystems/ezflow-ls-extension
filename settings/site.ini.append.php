252e14ba9f9fbbd249e8370ab993e67a6d04a6c7
Lukasz Serwatka <ls@ez.no>
2007-12-03 14:10:16 +0000

    - Moved to ezflow extension.
    git-svn-id: http://svn.ez.no/svn/extensions/ezflow/trunk@2345 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ settings/site.ini.append.php
@@ -0,0 +1,13 @@
+<?php /*
+
+[TemplateSettings]
+ExtensionAutoloadPath[]=ezflow
+
+[DesignSettings]
+AdditionalSiteDesignList[]=standard
+
+[SiteAccessSettings]
+SiteList[]=iphone
+AvailableSiteAccessList[]=iphone
+
+*/ ?>
